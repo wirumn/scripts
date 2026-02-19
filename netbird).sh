75c2b42bb9671172fec54cc12d80d2663470255e
actually not <wirumn@gmail.com>
2026-02-19 13:30:39 +0200

    Delete netbird).sh

--- netbird).sh
+++
@@ -1,7 +0,0 @@
-netbird)
-    name="NetBird"
-    type="pkg"
-    downloadURL="https://pkgs.netbird.io/macos/universal"
-    appNewVersion=$(curl -LsI $downloadURL -o /dev/null -w '%{url_effective}' | grep -oE "\d+\.\d+\.\d+")
-    expectedTeamID="TA739QLA7A"
-    ;;
